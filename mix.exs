78261d340eeeb20a41b7fcedd897e82acf602059
Johannes Christ <jc@jchri.st>
2019-04-12 23:42:08 +0200

    Add `credo` lint dependency.

--- mix.exs
+++ mix.exs
@@ -35,7 +35,10 @@ defmodule CrowPlugins.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:crow, "~> 0.1"}
+      {:crow, "~> 0.1"},
+
+      # Linting dependencies
+      {:credo, "~> 1.0", only: :dev, runtime: false},
     ]
   end
 end
